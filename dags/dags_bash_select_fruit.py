69038da8f52cc0993ea845d3426efac2f72832d0
yeonjae <helloyj12@naver.com>
2023-06-18 19:17:29 +0900

    select_fruit.sh 수행하는 dag 수정

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -1,6 +1,5 @@
 from airflow import DAG
 import pendulum
-import datetime
 from airflow.operators.bash import BashOperator
 
 with DAG(
@@ -12,12 +11,12 @@ with DAG(
     
     t1_orange = BashOperator(
         task_id="t1_orange",
-        bash_command="opt/airflow/plugins/shell/select_fruit.sh ORANGE",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE",
     )
 
     t2_avocado = BashOperator(
         task_id="t2_avocado",
-        bash_command="opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
     )
 
     t1_orange >> t2_avocado
