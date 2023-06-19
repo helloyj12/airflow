8707ff6b16b1d47f0e39cbbef04e5be6abfc9eca
yeonjae <helloyj12@naver.com>
2023-06-19 17:28:58 +0900

    email operator 업로드

---
+++ dags/dags_email_operator.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.email import EmailOperator
+
+with DAG(
+    dag_id="dags_email_operator",
+    schedule="0 8 1 * *", # 매월 1일 아침 8시
+    start_date=pendulum.datetime(2023, 6, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    send_email_task = EmailOperator(
+        task_id='send_email_task',
+        to='yeonj_kim@naver.com',
+        cc='helloyj12@naver.com' # 참조
+        subject='Airflow 성공 알림'
+        html_content='Airflow 작업이 완료되었습니다.'
+    )
