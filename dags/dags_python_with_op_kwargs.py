0553428cb7888d629474ca53598f824a4d261613
yeonjae <helloyj12@naver.com>
2023-06-23 15:33:02 +0900

    op_kwargs DAG 수정

--- dags/dags_python_with_op_kwargs.py
+++ dags/dags_python_with_op_kwargs.py
@@ -5,7 +5,7 @@ from common.common_func import regist2
 import pendulum
 
 with DAG(
-    dag_id="dags_python_with_op_args",
+    dag_id="dags_python_with_op_kwargs",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 6, 1, tz="Asia/Seoul"),
     catchup=False
@@ -14,6 +14,6 @@ with DAG(
     regist2_t1 = PythonOperator(
         task_id='regist2_t1',
         python_callable=regist2,
-        op_args=['yeonjaekim', 'female', 'korea', 'jeju']
+        op_args=['yeonjaekim', 'female', 'korea', 'jeju'],
         op_kwargs={'email':'yeonj.kim012@gmail.com', 'phone':'010-2345-9876'}
     )
