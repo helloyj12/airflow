d29aa8f12e9f28afefdc26ceaf1f16c60feeaa74
yeonjae <helloyj12@naver.com>
2023-07-06 15:40:54 +0900

    python macro DAG 수정

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -15,7 +15,7 @@ with DAG(
             templates_dict={'start_date':
                             '{{(data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months=-1, day=1)) | ds}}',
                             'end_date':
-                            '{{(data_interval_den.in_timezone("Asia/Seoul").replace(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)) | ds}}'})
+                            '{{(data_interval_end.in_timezone("Asia/Seoul").replace(day=1) + macros.dateutil.relativedelta.relativedelta(days=-1)) | ds}}'})
     def get_datetime_macro(**kwargs):
         templates_dict = kwargs.get('templates_dict') or {}
         if templates_dict:
