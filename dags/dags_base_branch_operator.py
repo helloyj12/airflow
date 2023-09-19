ee311dd2f62c28075c1a6dc257fd801b6986a844
yeonjae <helloyj12@naver.com>
2023-09-19 18:14:24 +0900

    base branch operator DAG 업로드

--- dags/dags_base_branch_operator.py
+++ dags/dags_base_branch_operator.py
@@ -12,6 +12,7 @@ with DAG(
         #overriding
         def choose_branch(self, context):
             import random
+            print(context)
 
             item_list = ['A', 'B', 'C']
             selected_item = random.choice(item_list)
